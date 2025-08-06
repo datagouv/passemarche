57824954e728acc716fb73be0891de6eafd95249
Thomas COMES <thomas.comes.tech@gmail.com>
2025-08-06 09:33:09 +0200

    feat: Extract market configuration logic into service layer

--- app/controllers/buyer/public_markets_controller.rb
+++ app/controllers/buyer/public_markets_controller.rb
@@ -21,52 +21,27 @@ module Buyer
     end
 
     def update
-      case step
-      when :setup
-        handle_setup_step
-      when :required_fields
-        handle_required_fields_step
-      when :additional_fields
-        handle_additional_fields_step
-      when :summary
-        @public_market.complete!
-        redirect_to finish_wizard_path, notice: t('buyer.public_markets.wizard_completed')
-      end
+      result = MarketConfigurationService.call(@public_market, step, step_params)
 
-      render_wizard @public_market
+      if step == :summary
+        redirect_to finish_wizard_path, notice: t('buyer.public_markets.wizard_completed')
+      else
+        jump_to(result[:next_step]) if result.is_a?(Hash) && result[:next_step]
+        render_wizard @public_market
+      end
     end
 
     private
 
-    def handle_setup_step
-      return if params[:public_market].blank?
-
-      return unless params[:public_market][:add_defense_market_type] == 'true'
-
-      return if @public_market.market_type_codes.include?('defense')
-
-      @public_market.market_type_codes << 'defense'
-      @public_market.save!
-    end
-
-    def handle_required_fields_step
-      @public_market.market_attributes = @presenter.available_required_market_attributes
-      @public_market.save!
-
-      jump_to(:additional_fields)
-    end
-
-    def handle_additional_fields_step
-      selected_attribute_keys = params[:selected_attribute_keys] || []
-      selected_optional_attributes = MarketAttribute.where(key: selected_attribute_keys)
-
-      existing_required_attributes = @public_market.market_attributes.required
-
-      all_attributes = (existing_required_attributes + selected_optional_attributes).uniq
-
-      @public_market.market_attributes = all_attributes
-      @public_market.save!
-      jump_to(:summary)
+    def step_params
+      case step
+      when :setup, :summary
+        params[:public_market] || {}
+      when :additional_fields
+        { selected_attribute_keys: params[:selected_attribute_keys] || [] }
+      else
+        {}
+      end
     end
 
     def find_public_market
