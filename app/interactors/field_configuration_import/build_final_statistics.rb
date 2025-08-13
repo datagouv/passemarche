b5be9c2c3968bf412a802522a5d36f9f0db12c80
Thomas COMES <thomas.comes.tech@gmail.com>
2025-08-13 22:04:46 +0200

    Introduce: FieldTranslationImport::BuildTranslationStatistics

--- app/interactors/field_configuration_import/build_final_statistics.rb
+++ app/interactors/field_configuration_import/build_final_statistics.rb
@@ -2,9 +2,23 @@
 
 class FieldConfigurationImport::BuildFinalStatistics < ApplicationInteractor
   def call
-    context.statistics[:total_active_attributes] = MarketAttribute.active.count
-    context.statistics[:total_market_types] = MarketType.active.count
-    context.statistics[:total_associations] = MarketType
+    context.statistics[:total_active_attributes] = count_active_attributes
+    context.statistics[:total_market_types] = count_active_market_types
+    context.statistics[:total_associations] = count_active_associations
+  end
+
+  private
+
+  def count_active_attributes
+    MarketAttribute.active.count
+  end
+
+  def count_active_market_types
+    MarketType.active.count
+  end
+
+  def count_active_associations
+    MarketType
       .active
       .joins(:market_attributes)
       .where(market_attributes: { deleted_at: nil })
