dc5d684fb7de50727d12ba4e6f2f26285523f1a4
Thomas COMES <thomas.comes.tech@gmail.com>
2025-08-22 15:24:44 +0200

    Refacto: Shared common syncable webhook behavior

--- app/jobs/market_application_webhook_job.rb
+++ app/jobs/market_application_webhook_job.rb
@@ -2,18 +2,12 @@
 
 # Webhook job for MarketApplication completion events
 class MarketApplicationWebhookJob < WebhookJob
+  include WebhookSyncable
+
   private
 
   def find_entity(entity_id)
     MarketApplication.find(entity_id)
-  end
-
-  def skip_delivery?(entity)
-    entity.sync_completed?
-  end
-
-  def before_delivery_callback(entity)
-    entity.update!(sync_status: :sync_processing)
   end
 
   def entity_payload(entity)
@@ -25,21 +19,5 @@ class MarketApplicationWebhookJob < WebhookJob
         identifier: entity.identifier
       }
     }
-  end
-
-  def entity_webhook_url(entity)
-    entity.editor.completion_webhook_url
-  end
-
-  def entity_webhook_secret(entity)
-    entity.editor.webhook_secret
-  end
-
-  def on_success_callback(entity)
-    entity.update!(sync_status: :sync_completed)
-  end
-
-  def on_error_callback(entity)
-    entity.update!(sync_status: :sync_failed)
   end
 end
