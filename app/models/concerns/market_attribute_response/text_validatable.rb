7b976d111432b4615b7a0353c218b38fa53b88d2
Thomas COMES <thomas.comes.tech@gmail.com>
2025-09-22 20:04:37 +0200

    Refacto: Splitting text behavior and Json validation

--- app/models/concerns/market_attribute_response/text_validatable.rb
+++ app/models/concerns/market_attribute_response/text_validatable.rb
@@ -3,31 +3,14 @@
 module MarketAttributeResponse::TextValidatable
   extend ActiveSupport::Concern
 
-  TEXT_MAX_LENGTH = 10_000
-
   included do
-    store_accessor :value, :text
-    validates :text, length: { maximum: TEXT_MAX_LENGTH }
-    validate :text_must_be_string
+    include MarketAttributeResponse::TextFieldBehavior
+
     validate :text_field_required
     validate :text_additional_properties_valid
   end
 
-  delegate :present?, to: :text, prefix: true
-
-  def validate_text_format(format, error_message)
-    return if text.blank?
-
-    errors.add(:text, error_message) unless text.match?(format)
-  end
-
   private
-
-  def text_must_be_string
-    return if text.nil? || text.is_a?(String)
-
-    errors.add(:text, I18n.t('activerecord.errors.json_schema.wrong_type'))
-  end
 
   def text_field_required
     return unless value.blank? || !value.key?('text')
