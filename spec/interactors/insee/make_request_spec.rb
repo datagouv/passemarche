2c9ca270e8f2a721e68db0874a836b1d8d52e9af
Thomas COMES <thomas.comes.tech@gmail.com>
2025-10-13 14:03:45 +0200

    Refacto: Extracting response from tests

--- spec/interactors/insee/make_request_spec.rb
+++ spec/interactors/insee/make_request_spec.rb
@@ -3,64 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe Insee::MakeRequest, type: :interactor do
+  include ApiResponses::InseeResponses
+
   let(:siret) { '41816609600069' }
   let(:base_url) { 'https://entreprise.api.gouv.fr/' }
   let(:token) { 'test_bearer_token_123' }
   let(:endpoint_url) { "#{base_url}v3/insee/sirene/etablissements/#{siret}" }
 
-  # Mock API response based on swagger specification
-  let(:successful_response_body) do
-    {
-      data: {
-        siret: '41816609600069',
-        siege_social: true,
-        etat_administratif: 'A',
-        date_fermeture: nil,
-        activite_principale: {
-          code: '6202A',
-          libelle: 'Conseil en systèmes et logiciels informatiques',
-          nomenclature: 'NAFRev2'
-        },
-        tranche_effectif_salarie: {
-          code: '21',
-          intitule: '50 à 99 salariés',
-          date_reference: '2022',
-          de: 50,
-          a: 99
-        },
-        status_diffusion: 'diffusible',
-        diffusable_commercialement: true,
-        enseigne: 'OCTO TECHNOLOGY',
-        unite_legale: {
-          siren: '418166096',
-          rna: nil,
-          siret_siege_social: '41816609600069',
-          type: 'personne_morale',
-          personne_morale_attributs: {
-            raison_sociale: 'OCTO TECHNOLOGY',
-            sigle: 'OCTO'
-          },
-          personne_physique_attributs: {
-            pseudonyme: nil,
-            prenom_usuel: nil,
-            prenom_1: nil,
-            prenom_2: nil,
-            prenom_3: nil,
-            prenom_4: nil,
-            nom_usage: nil,
-            nom_naissance: nil,
-            sexe: nil
-          },
-          categorie_entreprise: 'PME',
-          status_diffusion: 'diffusible'
-        }
-      },
-      meta: {
-        date_derniere_mise_a_jour: 1_704_067_200
-      },
-      links: {}
-    }.to_json
-  end
+  let(:successful_response_body) { insee_etablissement_success_response(siret:) }
 
   before do
     allow(Rails.application.credentials).to receive_message_chain(:api_entreprise, :base_url).and_return(base_url)
