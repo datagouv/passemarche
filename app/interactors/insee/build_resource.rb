3cf9bfece9fc0331873bbfa2d9b4ccb2f848dd18
Thomas COMES <thomas.comes.tech@gmail.com>
2025-10-15 11:40:52 +0200

    INSEE: added additionnal fields

--- app/interactors/insee/build_resource.rb
+++ app/interactors/insee/build_resource.rb
@@ -3,8 +3,28 @@ class Insee::BuildResource < BuildResource
 
   def resource_attributes
     {
-      siret: json_body['siret'],
-      category: json_body.dig('unite_legale', 'categorie_entreprise')
+      siret:,
+      category:,
+      main_activity:,
+      social_reason:
     }
+  end
+
+  private
+
+  def siret
+    json_body['siret']
+  end
+
+  def category
+    json_body.dig('unite_legale', 'categorie_entreprise')
+  end
+
+  def main_activity
+    json_body.dig('activite_principale', 'libelle')
+  end
+
+  def social_reason
+    json_body.dig('unite_legale', 'personne_morale_attributs', 'raison_sociale')
   end
 end
