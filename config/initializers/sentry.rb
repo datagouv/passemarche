b012e9b2953947c6a4e6a841c1e4c9500e129901
Thomas COMES <thomas.comes.tech@gmail.com>
2025-10-21 15:37:28 +0200

    Added default configuration by environments

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.sentry_dsn
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-  config.enabled_environments = %w[sandbox]
+  config.enabled_environments = %w[sandbox staging preprod production]
 
   config.traces_sample_rate = 1.0
 end
