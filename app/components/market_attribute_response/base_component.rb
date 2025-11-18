43cae4bd59346b93f27c0eea70fc23d4d45f90a6
Thomas COMES <thomas.comes.tech@gmail.com>
2025-11-18 12:23:07 +0100

    lookbook: Added base component

---
+++ app/components/market_attribute_response/base_component.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class MarketAttributeResponse::BaseComponent < ViewComponent::Base
+  attr_reader :market_attribute_response, :form, :context
+
+  delegate :market_attribute, to: :market_attribute_response
+  delegate :auto?, :manual_after_api_failure?, to: :market_attribute_response
+
+  def initialize(market_attribute_response:, form: nil, context: :web)
+    @market_attribute_response = market_attribute_response
+    @form = form
+    @context = context
+  end
+
+  def form_mode?
+    form.present?
+  end
+
+  def display_mode?
+    !form_mode?
+  end
+
+  def manual?
+    market_attribute_response.manual? || market_attribute_response.manual_after_api_failure?
+  end
+
+  def show_value?
+    !auto? || context == :buyer
+  end
+
+  def field_label
+    I18n.t(
+      "form_fields.candidate.fields.#{market_attribute.key}.name",
+      default: market_attribute.key.humanize
+    )
+  end
+
+  def field_description
+    I18n.t(
+      "form_fields.candidate.fields.#{market_attribute.key}.description",
+      default: nil
+    )
+  end
+
+  def auto_filled_message
+    I18n.t('candidate.market_applications.auto_filled_message')
+  end
+end
