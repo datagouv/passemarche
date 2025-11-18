37789da86f984681051f9e6ee32b497ba43b79be
Thomas COMES <thomas.comes.tech@gmail.com>
2025-11-18 12:28:28 +0100

    Added source badge components

---
+++ app/components/source_badge_component.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class SourceBadgeComponent < ViewComponent::Base
+  attr_reader :market_attribute_response, :inline
+
+  def initialize(market_attribute_response:, inline: false)
+    @market_attribute_response = market_attribute_response
+    @inline = inline
+  end
+
+  def render?
+    market_attribute_response.auto? || market_attribute_response.manual_after_api_failure?
+  end
+
+  def badge_text
+    if market_attribute_response.auto?
+      I18n.t('candidate.market_applications.badges.data_from_api')
+    elsif market_attribute_response.manual_after_api_failure?
+      I18n.t('candidate.market_applications.badges.manual_after_api_failure',
+        default: 'Renseigné après échec API')
+    end
+  end
+
+  def badge_css_class
+    if market_attribute_response.auto?
+      'fr-badge fr-badge--success fr-badge--sm'
+    elsif market_attribute_response.manual_after_api_failure?
+      'fr-badge fr-badge--warning fr-badge--sm'
+    end
+  end
+end
