9b2eafa91879374681c120646a88932f27925886
Camille Regnault <c.regnault.pro@gmail.com>
2026-01-05 15:43:08 +0100

    fix label

--- features/step_definitions/motifs_exclusion_contestation_steps.rb
+++ features/step_definitions/motifs_exclusion_contestation_steps.rb
@@ -2,7 +2,7 @@
 
 When('I fill in the contestation form with a file and text') do
   bloc = find(:xpath, "//div[contains(@class, 'fr-notice') and .//h3[contains(text(), 'liquidation')]]")
-  bloc.fill_in 'Décrivez votre situation', with: 'Explication de la contestation.'
+  bloc.fill_in 'Décrivez les motifs de votre contestation', with: 'Explication de la contestation.'
   bloc.attach_file('market_attribute_response_file', Rails.root.join('spec/fixtures/files/test.pdf'), make_visible: true) if bloc.has_field?('market_attribute_response_file', type: 'file')
 end
 
@@ -18,5 +18,5 @@ end
 
 Then('I should see the contestation form') do
   bloc = find(:xpath, "//div[contains(@class, 'fr-notice') and .//h3[contains(text(), 'liquidation')]]")
-  expect(bloc).to have_content('Décrivez votre situation')
+  expect(bloc).to have_content('Décrivez les motifs de votre contestation')
 end
