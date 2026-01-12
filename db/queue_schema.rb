eb802c51bb3be61e965251e6abf90689a0f0394f
Camille Regnault <c.regnault.pro@gmail.com>
2026-01-12 14:13:14 +0100

    add hidden attribute

--- db/queue_schema.rb
+++ db/queue_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_13_163210) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_19_154222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -102,6 +102,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_13_163210) do
 
   create_table "market_attribute_responses", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.boolean "hidden", default: false
     t.bigint "market_application_id", null: false
     t.bigint "market_attribute_id", null: false
     t.integer "source", default: 0, null: false
