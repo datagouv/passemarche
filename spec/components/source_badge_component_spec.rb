b93384b399a8350ce649de1925747a1ea7a3506b
Thomas COMES <thomas.comes.tech@gmail.com>
2026-01-19 10:33:32 +0100

    Fix display badges from the tabs
    Enhanced source badge component to accept an explicit source: parameter, allowing badges to be rendered per-cell instead of only per-response

--- spec/components/source_badge_component_spec.rb
+++ spec/components/source_badge_component_spec.rb
@@ -32,6 +32,32 @@ RSpec.describe SourceBadgeComponent, type: :component do
         expect(component.render?).to be false
       end
     end
+
+    context 'with explicit source parameter' do
+      it 'returns true when source is :auto' do
+        component = described_class.new(source: :auto)
+
+        expect(component.render?).to be true
+      end
+
+      it 'returns true when source is :manual_after_api_failure' do
+        component = described_class.new(source: :manual_after_api_failure)
+
+        expect(component.render?).to be true
+      end
+
+      it 'returns false when source is nil' do
+        component = described_class.new(source: nil)
+
+        expect(component.render?).to be false
+      end
+
+      it 'returns false without any parameters' do
+        component = described_class.new
+
+        expect(component.render?).to be false
+      end
+    end
   end
 
   describe '#badge_text' do
@@ -58,6 +84,26 @@ RSpec.describe SourceBadgeComponent, type: :component do
         expect(component.badge_text).to eq('Déclaré sur l\'honneur')
       end
     end
+
+    context 'with explicit source parameter' do
+      it 'returns i18n translation for data_from_api when source is :auto' do
+        component = described_class.new(source: :auto)
+
+        allow(I18n).to receive(:t).with('candidate.market_applications.badges.data_from_api')
+          .and_return('Données API')
+
+        expect(component.badge_text).to eq('Données API')
+      end
+
+      it 'returns i18n translation for declared_on_honor when source is :manual_after_api_failure' do
+        component = described_class.new(source: :manual_after_api_failure)
+
+        allow(I18n).to receive(:t).with('candidate.market_applications.badges.declared_on_honor')
+          .and_return('Déclaré sur l\'honneur')
+
+        expect(component.badge_text).to eq('Déclaré sur l\'honneur')
+      end
+    end
   end
 
   describe '#badge_css_class' do
@@ -74,6 +120,20 @@ RSpec.describe SourceBadgeComponent, type: :component do
       it 'returns info badge classes' do
         response = create(:market_attribute_response_text_input, market_attribute:, source: :manual_after_api_failure, value: { 'text' => 'Test' })
         component = described_class.new(market_attribute_response: response)
+
+        expect(component.badge_css_class).to eq('fr-badge fr-badge--info fr-badge--sm')
+      end
+    end
+
+    context 'with explicit source parameter' do
+      it 'returns success badge classes when source is :auto' do
+        component = described_class.new(source: :auto)
+
+        expect(component.badge_css_class).to eq('fr-badge fr-badge--success fr-badge--sm')
+      end
+
+      it 'returns info badge classes when source is :manual_after_api_failure' do
+        component = described_class.new(source: :manual_after_api_failure)
 
         expect(component.badge_css_class).to eq('fr-badge fr-badge--info fr-badge--sm')
       end
@@ -116,6 +176,32 @@ RSpec.describe SourceBadgeComponent, type: :component do
       it 'does not render anything' do
         response = create(:market_attribute_response_text_input, market_attribute:, source: :manual, value: { 'text' => 'Test' })
         component = described_class.new(market_attribute_response: response)
+
+        render_inline(component)
+
+        expect(rendered_content).to be_blank
+      end
+    end
+
+    context 'with explicit source parameter' do
+      it 'renders badge with success class when source is :auto' do
+        component = described_class.new(source: :auto)
+
+        render_inline(component)
+
+        expect(page).to have_css('span.fr-badge.fr-badge--success.fr-badge--sm')
+      end
+
+      it 'renders badge with info class when source is :manual_after_api_failure' do
+        component = described_class.new(source: :manual_after_api_failure)
+
+        render_inline(component)
+
+        expect(page).to have_css('span.fr-badge.fr-badge--info.fr-badge--sm')
+      end
+
+      it 'does not render anything without parameters' do
+        component = described_class.new
 
         render_inline(component)
 
