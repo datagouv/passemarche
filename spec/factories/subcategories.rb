9d89f4bbc2a3a9b3d33a3cc453bf812320feebec
Thomas COMES <thomas.comes.tech@gmail.com>
2026-02-06 16:13:15 +0100

    Add Category and Subcategory models with specs
    Category has soft delete with cascade to subcategories.
    Subcategory restricts destruction when market_attributes exist.
    MarketAttribute gains optional belongs_to :subcategory.

---
+++ spec/factories/subcategories.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :subcategory do
+    association :category
+    sequence(:key) { |n| "subcategory_#{n}" }
+    position { 0 }
+
+    trait :with_labels do
+      buyer_label { "Buyer #{key.humanize}" }
+      candidate_label { "Candidate #{key.humanize}" }
+    end
+
+    trait :inactive do
+      deleted_at { Time.current }
+    end
+  end
+end
