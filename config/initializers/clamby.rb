1ded51aed3f8289910fbf3884f9c9f0184aed3e4
Camille Regnault <c.regnault.pro@gmail.com>
2026-02-06 16:36:59 +0100

    update config for production

--- config/initializers/clamby.rb
+++ config/initializers/clamby.rb
@@ -1,9 +1,15 @@
-Clamby.configure({
+# frozen_string_literal: true
+
+clamscan_path = Rails.application.credentials.dig(:clamav, :clamscan_path)
+
+Clamby.configure(
   check: false,
   daemonize: false,
-  clamscan_path: '/opt/homebrew/bin/clamscan',
-  error_clamscan_missing: false,
-  error_clamscan_client_error: false,
+  clamscan_path:,
+  # --- Security ---
+  error_file_virus: true,
   error_file_missing: true,
-  error_file_virus: false
-})
+  # --- Infra ---
+  error_clamscan_missing: false,
+  error_clamscan_client_error: false
+)
