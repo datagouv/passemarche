90bd64d2e8da9b5b7df2656cc059dea792e01fbe
Thomas COMES <thomas.comes.tech@gmail.com>
2026-02-14 11:08:44 +0100

    Add reorder endpoint for socle de base attributes
    PATCH /admin/socle_de_base/reorder accepts ordered_ids and
    updates MarketAttribute positions in a transaction.
    
    Part of FAS-419.

--- app/controllers/admin/socle_de_base_controller.rb
+++ app/controllers/admin/socle_de_base_controller.rb
@@ -5,4 +5,16 @@ class Admin::SocleDeBaseController < Admin::ApplicationController
     @market_attributes = MarketAttribute.active.ordered.includes(:market_types)
     @stats = SocleDeBaseStatsService.call
   end
+
+  def reorder
+    ordered_ids = params.expect(ordered_ids: [])
+
+    MarketAttribute.transaction do
+      ordered_ids.each_with_index do |id, index|
+        MarketAttribute.where(id:).update_all(position: index) # rubocop:disable Rails/SkipsModelValidations
+      end
+    end
+
+    head :ok
+  end
 end
