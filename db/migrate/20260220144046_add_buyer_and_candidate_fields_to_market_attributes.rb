9a222c415d154351b9136ae444f02f148c2aa31a
Thomas COMES <thomas.comes.tech@gmail.com>
2026-02-23 15:08:38 +0100

    Fixing migration

--- db/migrate/20260220144046_add_buyer_and_candidate_fields_to_market_attributes.rb
+++ db/migrate/20260220144046_add_buyer_and_candidate_fields_to_market_attributes.rb
@@ -1,10 +1,10 @@
 class AddBuyerAndCandidateFieldsToMarketAttributes < ActiveRecord::Migration[8.1]
   def change
     change_table :market_attributes, bulk: true do |t|
-      t.string :buyer_name, if_not_exists: true
-      t.text :buyer_description, if_not_exists: true
-      t.string :candidate_name, if_not_exists: true
-      t.text :candidate_description, if_not_exists: true
+      t.string :buyer_name unless t.column_exists?(:buyer_name)
+      t.text :buyer_description unless t.column_exists?(:buyer_description)
+      t.string :candidate_name unless t.column_exists?(:candidate_name)
+      t.text :candidate_description unless t.column_exists?(:candidate_description)
     end
   end
 end
