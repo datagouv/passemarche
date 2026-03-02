48ae4b88e7ebe47acbfe01c2d30c7a2f9913db3f
Camille Regnault <c.regnault.pro@gmail.com>
2026-03-02 19:12:12 +0100

    feat(auth): create users table migration

--- db/queue_schema.rb
+++ db/queue_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_20_144046) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_02_000001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -360,6 +360,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_20_144046) do
     t.index ["category_id"], name: "index_subcategories_on_category_id"
     t.index ["deleted_at"], name: "index_subcategories_on_deleted_at"
     t.index ["position"], name: "index_subcategories_on_position"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "authentication_token_sent_at"
+    t.datetime "created_at", null: false
+    t.string "email", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "versions", force: :cascade do |t|
