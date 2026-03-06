ebf3e46ab13d1ae9b4db5558961338996c55f9fb
Camille Regnault <c.regnault.pro@gmail.com>
2026-03-06 09:39:27 +0100

    feat: wire Candidate::Authentication concern and require_candidate_authentication guard

--- app/controllers/candidate/market_applications_controller.rb
+++ app/controllers/candidate/market_applications_controller.rb
@@ -6,6 +6,7 @@ module Candidate
 
     prepend_before_action :set_steps
     prepend_before_action :find_market_application
+    before_action :require_candidate_authentication
     before_action :check_application_not_completed, except: [:retry_sync]
     before_action :set_wizard_steps
 
