c62fab3234b8156d80bdf31f1f37a535a229dcfe
Camille Regnault <c.regnault.pro@gmail.com>
2026-03-26 08:13:40 +0100

    update errors for siret and email and add translations

--- app/interactors/candidate/validate_siret.rb
+++ app/interactors/candidate/validate_siret.rb
@@ -5,9 +5,11 @@ module Candidate
     delegate :siret, to: :context
 
     def call
-      return if SiretValidator.valid?(siret)
-
-      context.fail!(errors: { siret: [I18n.t('errors.messages.invalid')] })
+      if siret.blank?
+        context.fail!(errors: { siret: [I18n.t('candidate.validations.siret_blank')] })
+      elsif !SiretValidator.valid?(siret)
+        context.fail!(errors: { siret: [I18n.t('candidate.validations.siret_invalid')] })
+      end
     end
   end
 end
