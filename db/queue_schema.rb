9991c60bc9db18b1d40fdc757d50f2f2df5f5a83
Camille Regnault <c.regnault.pro@gmail.com>
2026-04-13 04:30:20 +0200

    add lot_limit field in public market creation
    clean schema

--- db/queue_schema.rb
+++ db/queue_schema.rb
@@ -249,6 +249,17 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_07_000003) do
     t.index ["sync_status"], name: "index_public_markets_on_sync_status"
   end
 
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.integer "byte_size", null: false
+    t.datetime "created_at", null: false
+    t.binary "key", null: false
+    t.bigint "key_hash", null: false
+    t.binary "value", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
+  end
+
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
     t.string "concurrency_key", null: false
     t.datetime "created_at", null: false
