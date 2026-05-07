7f29b4b5221ee11f339c08af1f56b4b52cb03d08
Camille Regnault <c.regnault.pro@gmail.com>
2026-05-07 11:55:59 +0200

    Update specs for buyer_return_url and candidate_return_url

--- spec/requests/candidate/sync_status_spec.rb
+++ spec/requests/candidate/sync_status_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Candidate::SyncStatus', type: :request do
-  let(:editor) { create(:editor, redirect_url:) }
-  let(:redirect_url) { nil }
+  let(:editor) { create(:editor, candidate_return_url:) }
+  let(:candidate_return_url) { nil }
   let(:public_market) { create(:public_market, :completed, editor:, sync_status: 'sync_completed') }
   let(:market_application) { create(:market_application, public_market:, siret: '73282932000074') }
   let(:user) { create(:user) }
@@ -34,8 +34,8 @@ RSpec.describe 'Candidate::SyncStatus', type: :request do
         get sync_status_path
       end
 
-      context 'when editor has redirect_url configured' do
-        let(:redirect_url) { 'https://editor.example.com/callback' }
+      context 'when editor has candidate_return_url configured' do
+        let(:candidate_return_url) { 'https://editor.example.com/callback' }
 
         it 'displays redirect link with both identifiers as query params' do
           expect(response.body).to include("market_identifier=#{public_market.identifier}")
@@ -43,8 +43,8 @@ RSpec.describe 'Candidate::SyncStatus', type: :request do
         end
       end
 
-      context 'when editor has no redirect_url configured' do
-        let(:redirect_url) { nil }
+      context 'when editor has no candidate_return_url configured' do
+        let(:candidate_return_url) { nil }
 
         it 'displays redirect link to root path' do
           expect(response.body).to include('href="/"')
