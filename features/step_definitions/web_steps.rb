c03701f4d2cd533b0627538944a412f6a3fb0be1
Camille Regnault <c.regnault.pro@gmail.com>
2026-06-04 18:26:00 +0200

    FAS-502 - Centraliser les steps génériques dans web_steps.rb

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+Then('I should see {string}') do |text|
+  expect(page).to have_content(text)
+end
+
 Then('I should not see {string}') do |text|
   expect(page).not_to have_content(text)
 end
