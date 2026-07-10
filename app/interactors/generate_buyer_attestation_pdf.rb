8b39e575347a43164755cef92c7aa52cfce8fcfa
Camille Regnault <c.regnault.pro@gmail.com>
2026-07-10 13:27:34 +0200

    refactor: extraire la génération PDF commune dans PdfGeneratable
    GenerateAttestationPdf et GenerateBuyerAttestationPdf dupliquaient
    le rendu, le filigrane, l'attachement et la gestion d'erreur. Le
    concern factorise ce socle commun pour permettre sa réutilisation
    par de futurs interactors de génération PDF.

--- app/interactors/generate_buyer_attestation_pdf.rb
+++ app/interactors/generate_buyer_attestation_pdf.rb
@@ -1,90 +1,54 @@
 # frozen_string_literal: true
 
 class GenerateBuyerAttestationPdf < ApplicationInteractor
+  include PdfGeneratable
+
   delegate :market_application, to: :context
 
   def call
     context.fail!(message: 'Attestation acheteur déjà générée') if market_application.buyer_attestation.attached?
 
-    generate_and_attach_pdf
+    generate_and_attach_pdf(
+      attachment: market_application.buyer_attestation,
+      template: 'buyer/attestations/show',
+      locals: { market_application:, transmission_time: },
+      filename:
+    )
     context.buyer_attestation = market_application.buyer_attestation
   end
 
   private
 
-  def generate_and_attach_pdf
-    pdf_content = generate_pdf_content
-
-    market_application.buyer_attestation.attach(
-      io: StringIO.new(pdf_content),
-      filename:,
-      content_type: 'application/pdf'
-    )
-  rescue ActionView::Template::Error,
-         ActionView::MissingTemplate,
-         ActiveStorage::Error,
-         Errno::ENOENT, Errno::EACCES,
-         RuntimeError => e
-    handle_error(e)
+  def transmission_time
+    @transmission_time ||= Time.zone.now.strftime('%d/%m/%Y à %H:%M')
   end
-
-  # rubocop:disable Metrics/AbcSize
-  def handle_error(error)
-    error_message = "Failed to generate buyer attestation PDF for application #{market_application.identifier}"
-
-    Rails.logger.error "#{error_message}: #{error.class} - #{error.message}"
-    Rails.logger.error error.backtrace.first(10).join("\n")
-
-    Sentry.capture_exception(
-      error,
-      extra: {
-        market_application_id: market_application.id,
-        market_application_identifier: market_application.identifier,
-        siret: market_application.siret,
-        public_market_id: market_application.public_market_id,
-        error_stage: 'buyer_attestation_pdf_generation',
-        error_class: error.class.name,
-        error_message: error.message
-      },
-      tags: {
-        component: 'pdf_generation',
-        document_type: 'buyer_attestation'
-      }
-    )
-
-    context.fail!(message: I18n.t('errors.market_application.buyer_attestation_generation_failed'))
-  end
-  # rubocop:enable Metrics/AbcSize
 
   def filename
     "buyer_attestation_FT#{market_application.identifier}.pdf"
   end
 
-  def generate_pdf_content
-    transmission_time = Time.zone.now.strftime('%d/%m/%Y à %H:%M')
+  def pdf_error_message
+    "Failed to generate buyer attestation PDF for application #{market_application.identifier}"
+  end
 
-    html_content = ApplicationController.render(
-      template: 'buyer/attestations/show',
-      formats: [:html],
-      layout: false,
-      locals: {
-        market_application:,
-        transmission_time:
-      }
-    )
+  def pdf_sentry_extra
+    {
+      market_application_id: market_application.id,
+      market_application_identifier: market_application.identifier,
+      siret: market_application.siret,
+      public_market_id: market_application.public_market_id
+    }
+  end
 
-    pdf_content = WickedPdf.new.pdf_from_string(
-      html_content,
-      page_size: 'A4',
-      margin: {
-        top: 20,
-        bottom: 20,
-        left: 15,
-        right: 15
-      },
-      print_media_type: true
-    )
+  def pdf_error_stage
+    'buyer_attestation_pdf_generation'
+  end
 
-    PdfWatermarkService.call(pdf_content)
+  def pdf_document_type
+    'buyer_attestation'
+  end
+
+  def pdf_failure_i18n_message
+    I18n.t('errors.market_application.buyer_attestation_generation_failed')
   end
 end
