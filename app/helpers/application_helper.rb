e74b41bc72b21dd4977f0e23b3a350731ac776ae
Camille Regnault <c.regnault.pro@gmail.com>
2026-07-15 17:10:24 +0200

    fix: colorer les badges de type dans la config des lots acheteur
    Les badges de scope (travaux/services/fournitures/commun) affichés dans
    la page de configuration des champs par lot restaient gris, alors que
    le wizard candidat et l'attestation utilisent déjà des badges colorés
    via market_type_badge_tag. On aligne le partial acheteur sur le même
    helper pour une apparence cohérente.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,18 +33,6 @@ module ApplicationHelper
     image_tag config[:icon], alt: '', aria: { hidden: true }, width: 36, height: 36, class: 'market-type-icon'
   end
 
-  def market_type_scope_tag(scope)
-    code = scope.to_s
-    config = MARKET_TYPE_CONFIGS[code]
-    content = if config
-                image_tag(config[:icon_only], alt: '', aria: { hidden: true }, width: 16, height: 16) +
-                  t("market_types.#{code}")
-              else
-                t("market_types.#{code}", default: code.humanize)
-              end
-    content_tag(:span, content, class: 'fr-tag fr-tag--sm')
-  end
-
   def commun_scope_icon_svg
     tag.img(src: svg_asset_data_uri('icon-commun-only.svg'), alt: '', aria: { hidden: true }, width: 12, height: 12)
   end
