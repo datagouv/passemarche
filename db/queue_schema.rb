99d98ce38a16acf478a94a53ba932106d716ca85
Camille Regnault <c.regnault.pro@gmail.com>
2026-08-06 18:42:13 +0200

    feat: étend GroupingMember pour la composition du groupement
    Ajoute company_name (résolue via API Entreprise à l'ajout d'un
    co-traitant), la validation d'unicité du SIRET dans un groupement, le
    rejet du SIRET du mandataire pour un co-traitant, et invitation_sent?
    pour distinguer les co-traitants déjà invités.

--- db/queue_schema.rb
+++ db/queue_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_29_100623) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_04_154855) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -84,6 +84,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_29_100623) do
   end
 
   create_table "grouping_members", force: :cascade do |t|
+    t.string "company_name"
     t.datetime "created_at", null: false
     t.string "email"
     t.bigint "grouping_id", null: false
