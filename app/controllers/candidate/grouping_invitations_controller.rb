ee87300670422c24551edcaa85586f4cc867c5e8
Camille Regnault <c.regnault.pro@gmail.com>
2026-09-10 14:46:51 +0200

    fix: protège la page publique d'invitation par le feature flag groupement
    GroupingInvitationsController n'incluait pas GroupementFeatureGuard,
    contrairement aux autres controllers du wizard groupement. Une
    invitation restait donc consultable même feature flag désactivé.

--- app/controllers/candidate/grouping_invitations_controller.rb
+++ app/controllers/candidate/grouping_invitations_controller.rb
@@ -2,6 +2,8 @@
 
 module Candidate
   class GroupingInvitationsController < ::ApplicationController
+    include Candidate::GroupementFeatureGuard
+
     def show
       @grouping_member = GroupingMember.find_by(invitation_token: params[:token])
       render plain: "Cette invitation n'a pas été trouvée", status: :not_found if @grouping_member.nil?
