0d0ca39e751138f4f712839bc680bb2c67fbb6db
Camille Regnault <c.regnault.pro@gmail.com>
2026-09-16 14:21:33 +0200

    refactor: extrait la logique du wizard groupement dans GroupementWizardSteps
    Le rebase sur FAS-561 faisait dépasser Metrics/ClassLength sur
    MarketApplication. Regroupe la logique des étapes du wizard groupement
    (next_required_wizard_step, step_after_application_mode, les prédicats
    *_choice_required?/*_applicable?) dans un concern dédié plutôt que
    d'étendre encore la classe principale.

--- app/models/market_application.rb
+++ app/models/market_application.rb
@@ -3,8 +3,7 @@
 class MarketApplication < ApplicationRecord
   include Completable
   include Syncable
-
-  POST_APPLICATION_MODE_STEPS = %i[lot_selection_mode grouping_legal_type grouping_composition].freeze
+  include GroupementWizardSteps
 
   has_paper_trail on: %i[update], only: [:completed_at]
 
@@ -51,54 +50,8 @@ class MarketApplication < ApplicationRecord
     user_id.nil? || user_id == user.id
   end
 
-  def application_mode_choice_required?
-    FeatureFlags::Groupement.enabled? && application_mode.nil?
-  end
-
-  def grouping_legal_type_choice_required?
-    return false if completed?
-
-    FeatureFlags::Groupement.enabled? && groupement? &&
-      Grouping.joins(:mandataire_market_application).exists?(legal_type: nil, market_applications: { id: })
-  end
-
-  def already_mandataire_elsewhere?
-    Grouping
-      .joins(:mandataire_market_application)
-      .where(public_market:, market_applications: { siret: })
-      .where.not(market_applications: { id: })
-      .exists?
-  end
-
-  def grouping_composition_choice_required?
-    return false unless FeatureFlags::Groupement.enabled? && groupement?
-
-    grouping = mandataire_grouping
-    return false if grouping.nil? || grouping.legal_type.nil?
-
-    grouping.grouping_members.co_traitant.none?(&:invitation_sent?)
-  end
-
-  def mandataire_grouping
-    Grouping.joins(:mandataire_market_application).find_by(market_applications: { id: })
-  end
-
   def mandataire_of_grouping?
     groupement? && !!grouping_member&.mandataire?
-  end
-
-  def lot_selection_mode_choice_required?
-    return false if completed?
-    return false unless lot_selection_mode_applicable?
-
-    solo = solo_counterpart
-    return lot_ids.empty? if solo.nil?
-
-    solo.lot_ids.empty? || lot_ids.empty?
-  end
-
-  def lot_selection_mode_applicable?
-    FeatureFlags::Groupement.enabled? && groupement? && public_market.lots.any?
   end
 
   def groupement_counterpart
@@ -111,23 +64,6 @@ class MarketApplication < ApplicationRecord
     return nil if solo?
 
     MarketApplication.where(public_market:, siret:, application_mode: :solo, user_id:).where.not(id:).first
-  end
-
-  def next_required_wizard_step
-    return nil unless FeatureFlags::Groupement.enabled?
-    return [self, :application_mode] if application_mode_choice_required?
-
-    target = groupement_counterpart || self
-    step = POST_APPLICATION_MODE_STEPS.find { |candidate| target.public_send(:"#{candidate}_choice_required?") }
-    step ? [target, step] : nil
-  end
-
-  def step_after_application_mode
-    target = groupement_counterpart || self
-    return [target, :company_identification] unless target.groupement?
-    return [target, :lot_selection_mode] if target.public_market.lots.any?
-
-    [target, :grouping_legal_type]
   end
 
   def update_api_status(api_name, status:, fields_filled: 0)
